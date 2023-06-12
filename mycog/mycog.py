2c0b53601dddc6b15b65f96264a7e63c828eb7bd
fakesmile723 <raoeswar723@gmail.com>
2023-06-12 16:51:30 +0530

    updated cog.py

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -9,6 +9,14 @@ class MyyCog(commands.Cog):
     @commands.command()
     async def madd(self, ctx, num1: int, num2: int):
          await ctx.send(num1 + num2)
-    @commands.hybrid_command()
-    async def madd(self, ctx, num1: int, num2: int):
-        await ctx.send(num1 + num2,  ephemeral=True)
+    # @commands.hybrid_command()
+    # async def madd(self, ctx, num1: int, num2: int):
+    #     await ctx.send(num1 + num2,  ephemeral=True)
+    @commands.command()
+    async def madd(self, ctx, *nums: int):
+        if len(nums) < 2:
+            await ctx.send("Please specify at least two numbers to add.", ephemeral=True)
+            return
+
+        result = sum(nums)
+        await ctx.send(f"The sum of {', '.join(map(str, nums))} is {result}.", ephemeral=True)
