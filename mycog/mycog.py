3bebd4bc5dff1c313f8d1dabcf6d310a5e9f959c
fakesmile723 <raoeswar723@gmail.com>
2023-06-12 17:08:42 +0530

    trail

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -1,18 +1,18 @@
-from redbot.core import commands
+# from redbot.core import commands
 
-class MyyCog(commands.Cog):
-    """My custom cog"""
+# class MyyCog(commands.Cog):
+#     """My custom cog"""
 
-    def __init__(self, bot):
-        self.bot = bot
+#     def __init__(self, bot):
+#         self.bot = bot
 
     # @commands.command()
     # async def madd(self, ctx, num1: int, num2: int):
     #      await ctx.send(num1 + num2)
-    @commands.hybrid_command()
-    async def madd(self, ctx, *nums: int):
-        result = sum(nums)
-        await ctx.send(f"The sum of {', '.join(map(str, nums))} is {result}.", ephemeral=True)
+    # @commands.hybrid_command()
+    # async def madd(self, ctx, *nums: int):
+    #     result = sum(nums)
+    #     await ctx.send(f"The sum of {', '.join(map(str, nums))} is {result}.", ephemeral=True)
 
     # @commands.hybrid_command()
     # async def madd(self, ctx, *nums: int):
@@ -22,3 +22,15 @@ class MyyCog(commands.Cog):
 
     #     result = sum(nums)
     #     await ctx.send(f"The sum of {', '.join(map(str, nums))} is {result}.", ephemeral=True)
+from redbot.core import commands
+
+class MyyCog(commands.Cog):
+    """My custom cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command()
+    async def madd(self, ctx, *nums: int):
+        result = sum(nums)
+        await ctx.send(f"The sum of {', '.join(map(str, nums))} is {result}.", ephemeral=True)
