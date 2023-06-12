260583beebaac222193205521a8f67bb6c41f7ba
fakesmile723 <raoeswar723@gmail.com>
2023-06-12 17:12:07 +0530

    trail

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -1,27 +1,3 @@
-# from redbot.core import commands
-
-# class MyyCog(commands.Cog):
-#     """My custom cog"""
-
-#     def __init__(self, bot):
-#         self.bot = bot
-
-    # @commands.command()
-    # async def madd(self, ctx, num1: int, num2: int):
-    #      await ctx.send(num1 + num2)
-    # @commands.hybrid_command()
-    # async def madd(self, ctx, *nums: int):
-    #     result = sum(nums)
-    #     await ctx.send(f"The sum of {', '.join(map(str, nums))} is {result}.", ephemeral=True)
-
-    # @commands.hybrid_command()
-    # async def madd(self, ctx, *nums: int):
-    #     if len(nums) < 2:
-    #         await ctx.send("Please specify at least two numbers to add.", ephemeral=True)
-    #         return
-
-    #     result = sum(nums)
-    #     await ctx.send(f"The sum of {', '.join(map(str, nums))} is {result}.", ephemeral=True)
 from redbot.core import commands
 
 class MyyCog(commands.Cog):
@@ -30,7 +6,14 @@ class MyyCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # @commands.command()
+    # async def madd(self, ctx, num1: int, num2: int):
+    #      await ctx.send(num1 + num2)
     @commands.hybrid_command()
     async def madd(self, ctx, *nums: int):
+        if len(nums) < 2:
+            await ctx.send("Please specify at least two numbers to add.", ephemeral=True)
+            return
+
         result = sum(nums)
         await ctx.send(f"The sum of {', '.join(map(str, nums))} is {result}.", ephemeral=True)
