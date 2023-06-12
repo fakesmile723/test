a5d00843bfe565e9213ace4b0940153e51b88d90
fakesmile723 <raoeswar723@gmail.com>
2023-06-13 00:46:31 +0530

    trail

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -1,7 +1,6 @@
 from redbot.core import commands
 import discord
 from discord.ui import Button, View
-from discord.ext import commands
 
 class MyyCog(commands.Cog):
     """My custom cog"""
@@ -9,18 +8,6 @@ class MyyCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # @commands.command()
-    # async def madd(self, ctx, num1: int, num2: int):
-    #      await ctx.send(num1 + num2)
-
-    # @commands.hybrid_command()
-    # async def madd(self, ctx, nums: commands.Greedy[int]):
-    #     if len(nums) < 2:
-    #         await ctx.send("Please specify at least two numbers to add.", ephemeral=True)
-    #         return
-
-    #     result = sum(list(nums))
-    #     await ctx.send(f"The sum of {', '.join(map(str, nums))} is {result}.", ephemeral=True)
     @commands.command()
     async def verify(ctx):
         button = Button(label="this is a test", style=discord.ButtonStyle.blurple)
