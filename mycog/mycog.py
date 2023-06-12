ad1a9e2f5b770f1cf93eca5ba4e2049642486b98
fakesmile723 <raoeswar723@gmail.com>
2023-06-13 00:42:46 +0530

    trail

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -1,4 +1,7 @@
 from redbot.core import commands
+import discord
+from discord.ui import Button, View
+from discord.ext import commands
 
 class MyyCog(commands.Cog):
     """My custom cog"""
@@ -10,11 +13,17 @@ class MyyCog(commands.Cog):
     # async def madd(self, ctx, num1: int, num2: int):
     #      await ctx.send(num1 + num2)
 
-    @commands.hybrid_command()
-    async def madd(self, ctx, nums: commands.Greedy[int]):
-        if len(nums) < 2:
-            await ctx.send("Please specify at least two numbers to add.", ephemeral=True)
-            return
+    # @commands.hybrid_command()
+    # async def madd(self, ctx, nums: commands.Greedy[int]):
+    #     if len(nums) < 2:
+    #         await ctx.send("Please specify at least two numbers to add.", ephemeral=True)
+    #         return
 
-        result = sum(list(nums))
-        await ctx.send(f"The sum of {', '.join(map(str, nums))} is {result}.", ephemeral=True)
+    #     result = sum(list(nums))
+    #     await ctx.send(f"The sum of {', '.join(map(str, nums))} is {result}.", ephemeral=True)
+    @commands.command()
+    async def verify(ctx):
+        button = Button(label="this is a test", style=discord.ButtonStyle.blurple)
+        view = View()
+        view.add_item(button)
+        await ctx.send("The button below is a test which wont work" , view=view)
