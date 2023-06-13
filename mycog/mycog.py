f257dc8e7b23556e0239e03929e9798efd824ed5
fakesmile723 <raoeswar723@gmail.com>
2023-06-13 23:44:37 +0530

    new mode

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -7,56 +7,56 @@ class MyyCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.load_message_id()
+        self.message_id = None
+        self.role_id = None
+        self.channel_id = None
 
-    def load_message_id(self):
-        try:
-            with open("message_id.txt", "r") as file:
-                self.message_id = int(file.read())
-        except FileNotFoundError:
-            self.message_id = None
+    @commands.command()
+    async def setverificationrole(self, ctx, role: discord.Role):
+        """Set the role ID for verification."""
+        self.role_id = role.id
+        await ctx.send(f"Verification role set to: {role.name}")
 
-    def save_message_id(self, message_id):
-        with open("message_id.txt", "w") as file:
-            file.write(str(message_id))
+    @commands.command()
+    async def setverificationchannel(self, ctx, channel: discord.TextChannel):
+        """Set the channel ID for verification messages."""
+        self.channel_id = channel.id
+        await ctx.send(f"Verification channel set to: {channel.mention}")
 
     @commands.command()
     async def verify(self, ctx):
-        embed = discord.Embed(
-            title="Verification",
-            description="**Read the rules and click on the button below that says Verify to gain access**",
-            color=0x2b2d31
-        )
+        if not self.role_id:
+            return await ctx.send("The verification role ID is not set. Please set it using the `set_verification_role` command.")
+
+        if not self.channel_id:
+            return await ctx.send("The verification channel ID is not set. Please set it using the `set_verification_channel` command.")
+
+        embed = discord.Embed(title="Verification", description="**Read the rules and click on the button below that says Verify to gain access**", color=0x2b2d31)
         embed.set_image(url="https://media.tenor.com/yG0BZ-wew-sAAAAC/verify-discord.gif")
 
-        vbutton = Button(
-            label="Verify",
-            style=discord.ButtonStyle.blurple,
-            emoji="<:yess:1020703229891330099>"
-        )
+        vbutton = Button(label="Verify", style=discord.ButtonStyle.blurple, emoji="<:yess:1020703229891330099>")
 
         async def button_callback(interaction):
             if self.message_id is None:
-                message = await interaction.response.send_message(embed=buttonembed, view=view2, ephemeral=True)
+                message = await ctx.guild.get_channel(self.channel_id).send(embed=buttonembed, view=view2, ephemeral=True)
                 self.message_id = message.id
-                self.save_message_id(message.id)
             else:
-                await interaction.response.edit_message(view=None)
+                existing_message = await ctx.guild.get_channel(self.channel_id).fetch_message(self.message_id)
+                await existing_message.edit(view=None)
+
         button1 = Button(emoji="👋🏻")
         button2 = Button(emoji="🔥")
         button3 = Button(emoji="🤩")
-        button4 = Button(emoji="✅")        
+        button4 = Button(emoji="✅")
 
         async def button1_callback(interaction):
-            await interaction.response.edit_message(content="this is button1", embed=None, view=None)
+            await interaction.response.edit_message(content="This is button1", embed=None, view=None)
 
         async def button2_callback(interaction):
             member = interaction.user
             guild = interaction.guild
 
-            role_id = 1118228001415508031
-
-            role = guild.get_role(role_id)
+            role = guild.get_role(self.role_id)
 
             if role is not None:
                 if role in member.roles:
@@ -67,12 +67,11 @@ class MyyCog(commands.Cog):
             else:
                 await interaction.response.edit_message(content="Role not found. Please contact a server administrator.", embed=None, view=None)
 
-
         async def button3_callback(interaction):
-            await interaction.response.edit_message(content="this is button3", embed=None, view=None)
+            await interaction.response.edit_message(content="This is button3", embed=None, view=None)
 
         async def button4_callback(interaction):
-            await interaction.response.edit_message(content="this is button4", embed=None, view=None)
+            await interaction.response.edit_message(content="This is button4", embed=None, view=None)
 
         button1.callback = button1_callback
         button2.callback = button2_callback
@@ -87,13 +86,10 @@ class MyyCog(commands.Cog):
 
         vbutton.callback = button_callback
 
-        buttonembed = discord.Embed(
-            title="Verifying",
-            description="You are about to verify yourself /n if you read the rules click on the right emote to get verified",
-            color=0x2b2d31
-        )
+        buttonembed = discord.Embed(title="Verifying", description="You are about to verify yourself.\nIf you read the rules, click on the right emote to get verified.", color=0x2b2d31)
 
         view = View()
         view.add_item(vbutton)
 
         await ctx.send(embed=embed, view=view)
+
